c8f1a49f31a9eead676e217d18d3b52616d90aa2
Nicole Pasquale <NSquale212@gmail.com>
2019-12-03 15:58:21 -0600

    Add changeStyle method

--- scripts.js
+++ scripts.js
@@ -17,7 +17,10 @@ class Die {
 		this.div.text(this.val);
 	}
 	reroll() {
-		this.div.click(() => this.roll());
+		this.div.click(() => {
+			this.removeStyle();
+			this.roll()
+		});
 	}
 	remove() {
 		this.div.dblclick(() => {
@@ -26,6 +29,50 @@ class Die {
 			Die.diceArray.splice(index, 1);
 		});
 	}
+	removeStyle() {
+		let cssClassArray = ['die-2', 'two', 'three', 'four-and-six', 'five'];
+		cssClassArray.forEach(style => this.div.removeClass(style));
+		this.div.addClass('die');
+	}
+	changeStyle() {
+		let span = $('<span class="dot"></span>');
+		let span2 = $('<span class="dot"></span><span class="dot"></span>');
+		let span3 = $('<span class="dot"></span><span class="dot"></span><span class="dot"></span>');
+		let span4 = $('<div class="column"><span class="dot"></span><span class="dot"></span></div><div class="column"><span class="dot"></span><span class="dot"></span></div>');
+		let span5 = $('<div class="column"><span class="dot"></span><span class="dot"></span></div><div class ="column"><span class="dot"></span></div><div class="column"><span class="dot"></span><span class="dot"></span></div>');
+		let span6 = $('<div class="column"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div><div class="column"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div>');
+		switch (this.val) {
+			case 1:
+				this.div.text('');
+				this.div.append(span);
+				break;
+			case 2:
+				this.div.text('');
+				this.div.removeClass('die').addClass('die-2 two');
+				this.div.append(span2);
+				break;
+			case 3:
+				this.div.text('');
+				this.div.removeClass('die').addClass('die-2 three');
+				this.div.append(span3);
+				break;
+			case 4:
+				this.div.text('');
+				this.div.removeClass('die').addClass('die-2 four-and-six');
+				this.div.append(span4);
+				break;
+			case 5:
+				this.div.text('');
+				this.div.removeClass('die').addClass('die-2 five');
+				this.div.append(span5);
+				break;
+			case 6:
+				this.div.text('');
+				this.div.removeClass('die').addClass('die-2 four-and-six');
+				this.div.append(span6);
+				break;
+		}
+	}
 }
 
 $('.new-die').click(() => {
@@ -33,12 +80,19 @@ $('.new-die').click(() => {
 });
 
 $('.roll-die').click(() => {
-	Die.diceArray.forEach(die => die.roll());
+	Die.diceArray.forEach(die => {
+		die.removeStyle();
+		die.roll();
+	});
 });
 
 $('.sum-die').click(() => {
-	let sum = Die.diceArray.reduce((total, num) => {
-		return total += num.val;
+	let sum = Die.diceArray.reduce((total, die) => {
+		return total += die.val;
 	}, 0);
 	Swal.fire(`The sum is: ${sum}`);
+});
+
+$(`.change-die`).click(() => {
+	Die.diceArray.forEach(die => die.changeStyle());
 });
